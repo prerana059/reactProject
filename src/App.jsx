7d0dbd542f04ab365f4fcf03be4b813d42cf9fa4
Prerana <prebhme@gmail.com>
2025-05-16 06:43:38 +0545

    my forst commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import List from "./List"
+
+
+function App() {
+  return (
+    <>
+    
+      <h1>Hello World!!!</h1>
+      <List/>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoList() {
+
+    return (
+        console.log (" My Project ")
+
+    );
+}
+
+export default TodoList
